896864f6ad903b20b34cc5c1124a450b3fd1eb5b
Sachin Saxena <sssaxena058@gmail.com>
2023-06-19 23:17:05 +0530

    saxena setup mvvmmmmmmm

--- app/src/main/java/com/example/mvvm/activity/ContainerActivity.kt
+++ app/src/main/java/com/example/mvvm/activity/ContainerActivity.kt
@@ -1,11 +1,14 @@
-package com.example.mvvm
+package com.example.mvvm.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvm.R
 
-class MainActivity : AppCompatActivity() {
+class ContainerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_container)
+
+
     }
 }
